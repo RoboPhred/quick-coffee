64263368cfc08dbff81b24f53b614fa4058a0b14
RoboPhred <robophred@gmail.com>
2018-11-07 11:21:06 -0700

    Include date ordered and status

--- packages/coffee-types/src/types/order.ts
+++ packages/coffee-types/src/types/order.ts
@@ -2,6 +2,8 @@ export interface ListOrderedItem {
   id: string;
   itemId: string;
   itemName: string;
+  dateOrdered: string;
+  status: "pending" | "in-progress" | "ready";
 }
 
 export interface OrderedItem extends ListOrderedItem {
