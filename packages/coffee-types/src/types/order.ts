0c4863f0efce135b246aa23d28e62973880d3bd8
RoboPhred <robophred@gmail.com>
2018-11-16 12:23:01 -0700

    Add order creator username to order item

--- packages/coffee-types/src/types/order.ts
+++ packages/coffee-types/src/types/order.ts
@@ -2,6 +2,7 @@ export interface OrderedItem {
   id: string;
   itemId: string;
   itemName: string;
+  orderCreatorUsername: string;
   orderDate: string;
   status: "pending" | "in-progress" | "ready";
   statusChangeDate: string;
