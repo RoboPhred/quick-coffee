e95019b9b4ac16c5cad8d6deba65914840b4a75b
RoboPhred <robophred@gmail.com>
2018-11-18 09:33:10 -0700

    Barista patch orders

--- packages/coffee-backend/src/endpoint/barista.ts
+++ packages/coffee-backend/src/endpoint/barista.ts
@@ -1,9 +1,12 @@
 import Router from "koa-router";
 
+import { PatchBaristaOrderRequest } from "coffee-types";
+
 import HttpStatusCodes from "http-status-codes";
 
+import { getAllOrders, updateStatus, getOrderById } from "../data/orders";
+
 import { authenticate } from "./auth";
-import { getAllOrders } from "../data/orders";
 
 const router = new Router({ prefix: "/barista" });
 router.use(authenticate("barista"));
@@ -14,6 +17,28 @@ router.get("/orders", async ctx => {
     orders: allOrders
   };
   ctx.status = HttpStatusCodes.OK;
+});
+
+router.patch("/orders/:orderId", async ctx => {
+  const { orderId } = ctx.params;
+  if (!orderId) {
+    ctx.status = HttpStatusCodes.BAD_REQUEST;
+    return;
+  }
+
+  const body = ctx.request.body;
+  if (body == null || typeof body !== "object") {
+    ctx.status = HttpStatusCodes.BAD_REQUEST;
+    return;
+  }
+
+  const { status } = body as PatchBaristaOrderRequest;
+  if (status) {
+    await updateStatus(orderId, status);
+  }
+
+  ctx.status = HttpStatusCodes.OK;
+  ctx.body = getOrderById(orderId);
 });
 
 export default router;
