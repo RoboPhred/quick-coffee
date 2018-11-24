922f3dfb79edfcae1e1fcf61553d75be109c6fa4
RoboPhred <robophred@gmail.com>
2018-11-24 10:30:40 -0700

    Use consistent jwt key when debugging

--- packages/coffee-backend/src/config.ts
+++ packages/coffee-backend/src/config.ts
@@ -1,5 +1,3 @@
-import uuidV4 from "uuid/v4";
-
-export const JWT_KEY = process.env.JWT_KEY || uuidV4();
+export const JWT_KEY = process.env.JWT_KEY || "development-key";
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
