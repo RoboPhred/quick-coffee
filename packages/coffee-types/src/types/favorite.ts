d5ef0ec40b995f5b509a43f98b5e616ed140b41c
RoboPhred <robophred@gmail.com>
2018-11-24 11:58:39 -0700

    Add options to favorite item

--- packages/coffee-types/src/types/favorite.ts
+++ packages/coffee-types/src/types/favorite.ts
@@ -1,6 +1,9 @@
+import { OrderOptions } from "./order";
+
 export interface FavoriteItem {
   id: number;
   favoriteName: string;
   itemId: number;
   itemName: string;
+  options: OrderOptions;
 }
