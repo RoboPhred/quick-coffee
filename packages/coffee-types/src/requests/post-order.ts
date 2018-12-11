ce9e913e2167885b8af1ec1c07068fe4828a4eb8
RoboPhred <robophred@gmail.com>
2018-12-11 13:55:26 -0700

    PostOrderRequest schema

--- packages/coffee-types/src/requests/post-order.ts
+++ packages/coffee-types/src/requests/post-order.ts
@@ -1,12 +1,33 @@
-import { OrderedItem, OrderOptions } from "../types";
+import { OrderedItem, OrderOptions, orderOptionsSchema } from "../types";
+import { JSONSchema6 } from "json-schema";
 
 export interface PostOrderRequest {
   order: OrderRequestItem;
 }
+export interface PostOrderResponse {
+  order: OrderedItem;
+}
+
 export interface OrderRequestItem {
   itemId: number;
   options: OrderOptions;
 }
-export interface PostOrderResponse {
-  order: OrderedItem;
-}
+export const orderRequestItemSchema: JSONSchema6 = {
+  title: "OrderRequestItem",
+  type: "object",
+  properties: {
+    itemId: { type: "number", minimum: 1 },
+    options: orderOptionsSchema
+  },
+  required: ["itemId", "options"]
+};
+Object.freeze(orderRequestItemSchema);
+
+export const postOrderRequestSchema: JSONSchema6 = {
+  title: "PostOrderRequest",
+  type: "object",
+  properties: {
+    order: orderRequestItemSchema
+  }
+};
+Object.freeze(postOrderRequestSchema);
